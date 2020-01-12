18a7a6748dc52422bdf1c86c7a5601c197ea2894
Susisu <susisu2413@yahoo.co.jp>
2020-01-12 17:41:07 +0900

    test: use jest.fn for callbacks

--- src/__tests__/subscription.spec.ts
+++ src/__tests__/subscription.spec.ts
@@ -3,15 +3,17 @@ import { Subscription, CompositeSubscription } from "../subscription";
 describe("Subscription", () => {
   describe("unsubscribe", () => {
     it("should call the function which is attached at the creation", () => {
-      let x = 0;
-      const subsciption = new Subscription(() => {
-        x += 1;
-      });
-      expect(x).toBe(0);
-      subsciption.unsubscribe();
-      expect(x).toBe(1);
-      subsciption.unsubscribe();
-      expect(x).toBe(1);
+      const callback = jest.fn();
+      const subscription = new Subscription(callback);
+
+      expect(callback).not.toHaveBeenCalled();
+
+      subscription.unsubscribe();
+      expect(callback).toHaveBeenCalledTimes(1);
+
+      // ubsubscribing twice or more has no effect
+      subscription.unsubscribe();
+      expect(callback).toHaveBeenCalledTimes(1);
     });
   });
 });
@@ -19,78 +21,78 @@ describe("Subscription", () => {
 describe("CompositeSubscription", () => {
   describe("add", () => {
     it("should add a subscription to the set of subscriptions", () => {
-      let x = 0;
-      const subscription = new Subscription(() => {
-        x += 1;
-      });
+      const callback = jest.fn();
+      const subscription = new Subscription(callback);
+
       const composite = new CompositeSubscription();
       composite.add(subscription);
-      expect(x).toBe(0);
+
+      expect(callback).not.toHaveBeenCalled();
+
       composite.unsubscribe();
-      expect(x).toBe(1);
-      composite.unsubscribe();
-      expect(x).toBe(1);
+      expect(callback).toHaveBeenCalledTimes(1);
     });
   });
 
   describe("remove", () => {
     it("should remove a subscription from the set", () => {
-      let x = 0;
-      const subscription = new Subscription(() => {
-        x += 1;
-      });
+      const callback = jest.fn();
+      const subscription = new Subscription(callback);
+
       const composite = new CompositeSubscription();
       composite.add(subscription);
       composite.remove(subscription);
-      expect(x).toBe(0);
+
+      expect(callback).not.toHaveBeenCalled();
+
       composite.unsubscribe();
-      expect(x).toBe(0);
+      expect(callback).not.toHaveBeenCalled();
     });
   });
 
   describe("clear", () => {
     it("should remove all the subsciptions from the set", () => {
-      let x1 = 0;
-      const subscription1 = new Subscription(() => {
-        x1 += 1;
-      });
-      let x2 = 0;
-      const subscription2 = new Subscription(() => {
-        x2 += 1;
-      });
+      const callback1 = jest.fn();
+      const subscription1 = new Subscription(callback1);
+      const callback2 = jest.fn();
+      const subscription2 = new Subscription(callback2);
+
       const composite = new CompositeSubscription();
       composite.add(subscription1);
       composite.add(subscription2);
       composite.clear();
-      expect(x1).toBe(0);
-      expect(x2).toBe(0);
+
+      expect(callback1).not.toHaveBeenCalled();
+      expect(callback2).not.toHaveBeenCalled();
+
       composite.unsubscribe();
-      expect(x1).toBe(0);
-      expect(x2).toBe(0);
+      expect(callback1).not.toHaveBeenCalled();
+      expect(callback2).not.toHaveBeenCalled();
     });
   });
 
   describe("unsubscribe", () => {
     it("should unsubscribe all the subsciptions in the set", () => {
-      let x1 = 0;
-      const subscription1 = new Subscription(() => {
-        x1 += 1;
-      });
-      let x2 = 0;
-      const subscription2 = new Subscription(() => {
-        x2 += 1;
-      });
+      const callback1 = jest.fn();
+      const subscription1 = new Subscription(callback1);
+      const callback2 = jest.fn();
+      const subscription2 = new Subscription(callback2);
+
       const composite = new CompositeSubscription();
       composite.add(subscription1);
       composite.add(subscription2);
-      expect(x1).toBe(0);
-      expect(x2).toBe(0);
+
+      expect(callback1).not.toHaveBeenCalled();
+      expect(callback2).not.toHaveBeenCalled();
+
       composite.unsubscribe();
-      expect(x1).toBe(1);
-      expect(x2).toBe(1);
+      expect(callback1).toHaveBeenCalledTimes(1);
+      expect(callback2).toHaveBeenCalledTimes(1);
+
+      // ubsubscribing twice or more has no effect
       composite.unsubscribe();
-      expect(x1).toBe(1);
-      expect(x2).toBe(1);
+      expect(callback1).toHaveBeenCalledTimes(1);
+      expect(callback2).toHaveBeenCalledTimes(1);
     });
   });
 });
